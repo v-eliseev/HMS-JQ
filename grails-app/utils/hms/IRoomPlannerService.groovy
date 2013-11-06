f10541dd15a70a1dfa00962442d6aefbe8e5c28c
evv <v-eliseev@yandex.ru>
2013-11-06 16:09:27 +0400

    bugfix

--- grails-app/utils/hms/IRoomPlannerService.groovy
+++ grails-app/utils/hms/IRoomPlannerService.groovy
@@ -2,7 +2,7 @@ package hms
 
 interface IRoomPlannerService {
 	
-	def convertData(def license, def roomCategories, def rooms, def reservations, def roomAssignments)
-	def callPlanner(def license, def dtoRoomCategories, def dtoRooms, def dtoReservations, def roomAssignments)
+	def convertData(def license, def roomCategories, def rooms, def reservations, def roomAssignments, def pricelist)
+	def callPlanner(def license, def dtoRoomCategories, def dtoRooms, def dtoReservations, def roomAssignments, def pricelist)
 	def convertResponse(def license, def dtoPlan)
 }

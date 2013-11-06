7a5f70bd47f0c1691d71f5c66f432825645e1922
evv <v-eliseev@yandex.ru>
2013-11-06 11:53:44 +0400

    pricelistitem

---
+++ test/unit/roomplanner/PricelistItemSpec.groovy
@@ -0,0 +1,20 @@
+package roomplanner
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(PricelistItem)
+class PricelistItemSpec extends Specification {
+
+	def setup() {
+	}
+
+	def cleanup() {
+	}
+
+	void "test something"() {
+	}
+}

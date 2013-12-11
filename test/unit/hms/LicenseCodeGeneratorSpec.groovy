22b566a9747f21eb0fe21653c9ab6808ed134a0a
evv <v-eliseev@yandex.ru>
2013-12-11 13:55:32 +0400

    mocking codecs in spock unit test for license generator

--- test/unit/hms/LicenseCodeGeneratorSpec.groovy
+++ test/unit/hms/LicenseCodeGeneratorSpec.groovy
@@ -1,27 +1,24 @@
 package hms
 
-import org.junit.*
 import grails.test.mixin.*
-import grails.test.GrailsUnitTestCase
+import grails.test.mixin.support.*
+import spock.lang.*
 
 import org.joda.time.DateTime
 import org.codehaus.groovy.grails.plugins.codecs.SHA1Codec
 import org.codehaus.groovy.grails.plugins.codecs.HexCodec
 
 /**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
-class Base32BytesCodecTests extends GrailsUnitTestCase {
+@TestMixin(GrailsUnitTestMixin)
+class LicenseCodeGeneratorSpec extends Specification {
 
-	@Before
-	protected void setUp() {
-		super.setUp()
-		loadCodec SHA1Codec
-		loadCodec HexCodec
-		loadCodec Base32BytesCodec
-	}
+	def 'test license code generation' () {
+		mockCodec SHA1Codec
+		mockCodec HexCodec
+		mockCodec Base32BytesCodec
 
-	void testCodec() {
 		def source = 'Vladislav Eliseev|v-eliseev@yandex.ru|' + new DateTime()
 		def shaCode = source.encodeAsSHA1()
 		def shortCode = shaCode.substring(0,32)

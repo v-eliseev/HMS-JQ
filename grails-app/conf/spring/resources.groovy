15baf3963274037d64870f006932e5ddf368d2cf
evv <v-eliseev@yandex.ru>
2014-11-13 15:19:12 +0300

    wurfl service added

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -35,4 +35,8 @@ beans = {
 	}
 	
 	securityEventListener(hms.auth.CustomSecurityEventListener)
+	
+	wurflHolder(net.sourceforge.wurfl.core.GeneralWURFLEngine, "classpath:/wurfl/wurfl-latest.zip") {
+	}
+	
 }
--- grails-app/services/hms/MobileService.groovy
+++ grails-app/services/hms/MobileService.groovy
@@ -1,11 +1,13 @@
 package hms
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpSession
-//import net.sourceforge.wurfl.core.Device
+
 import org.apache.commons.lang.StringUtils
 import org.codehaus.groovy.grails.web.context.ServletContextHolder
 import org.springframework.web.context.support.WebApplicationContextUtils
 import org.springframework.context.ApplicationContext
+
+import net.sourceforge.wurfl.core.Device
 
 /**
  * MobileDecoratorMapper
@@ -13,7 +15,8 @@ import org.springframework.context.ApplicationContext
  * @author gdaley
  */
 class MobileService {
-	//def wurflManager
+	
+	def wurflHolder
 
 	def isMobileUser(HttpServletRequest request) {
 		boolean isMobile = false
@@ -52,28 +55,23 @@ class MobileService {
 	}
 
 	def isMobileBrowser(HttpServletRequest request) {
-		def device = request.getAttribute('currentDevice')
-		boolean detected = false // TODO device.isMobile()
-		if (detected) {
-			def userAgent = request.getHeader('User-Agent')
-			if (userAgent?.contains('iPad')) { // skip iPads
-				detected = false
-			} else if (userAgent?.contains('Android') && !userAgent?.contains('Mobile')) {
-				// and android tables
-				detected = false
-			}
-		}
-		detected
-	}
+//		def device = request.getAttribute('currentDevice')
+//		boolean detected = false // TODO device.isMobile()
+//		if (detected) {
+//			def userAgent = request.getHeader('User-Agent')
+//			if (userAgent?.contains('iPad')) { // skip iPads
+//				detected = false
+//			} else if (userAgent?.contains('Android') && !userAgent?.contains('Mobile')) {
+//				// and android tables
+//				detected = false
+//			}
+//		}
+//		detected
+//	}
 
-/*
-	if (wurflManager == null) {
-		def ctx = WebApplicationContextUtils.getWebApplicationContext(ServletContextHolder.servletContext)
-		wurflManager = ctx.getBean("wurflManager")
-	}
 
-	def Device device = wurflManager.getDeviceForRequest(request)
-	def String capability = device.getCapability("mobile_browser")
-	return StringUtils.isNotBlank(capability)
-*/	
+		Device device = wurflHolder.getDeviceForRequest(request)
+		def capability = device.getCapability("mobile_browser")
+		return StringUtils.isNotBlank(capability)
+	}		
 }
